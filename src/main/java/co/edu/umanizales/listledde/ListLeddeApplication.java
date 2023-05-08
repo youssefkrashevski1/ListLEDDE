be559e1d0d576ded832f99e1dd16bbcd7b59fde6
youssefkrashevski1 <127439429+youssefkrashevski1@users.noreply.github.com>
2023-05-08 18:09:24 -0500

    Initial commit

---
+++ src/main/java/co/edu/umanizales/listledde/ListLeddeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.listledde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListLeddeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListLeddeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/umanizales/listledde/ListLeddeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.listledde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListLeddeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
